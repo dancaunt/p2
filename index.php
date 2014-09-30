5861134bd361b435b0da0d261630d48b0ef344bb
Dan <dcaunt@fas.harvard.edu>
2014-09-29 20:18:21 -0400

    Started index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+
+<title>Daniel Caunt P2 - xkcd Password Generator</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel='stylesheet' href='bootstrap.min.css' type='text/css'>
+<link href="bootstrapflatly.min.css" rel="stylesheet">
+
+<style>
+ h1 {
+  text-align:center;
+ }
+ .container {
+	width:50%;
+	min-width:600px;
+	margin-bottom:50px;
+ }	
+ img {
+	display:block;
+	margin:auto;
+	width:33%;
+ }
+</style>
+
+</head>
+<body>
+
+<div class="container">
+<h1>xkcd Password Generator</h1>
+
+<a href='http://xkcd.com/936/'>xkcd password strength</a><br>
+
+<a href='http://xkcd.com/936'><img src='http://imgs.xkcd.com/comics/password_strength.png'></a>
+
+</div>
+</body>
+</html>
